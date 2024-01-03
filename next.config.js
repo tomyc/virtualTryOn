96f95401b950d08c3980b3c2ce78e9dac662f160
tomyc <tomyc@users.noreply.github.com>
2024-01-03 09:40:47 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ next.config.js
@@ -0,0 +1,59 @@
+const { webpack } = require("next/dist/compiled/webpack/webpack");
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve = {
+        ...config.resolve,
+        fallback: {
+          // fixes proxy-agent dependencies
+          net: false,
+          dns: false,
+          tls: false,
+          assert: false,
+          // fixes next-i18next dependencies
+          path: false,
+          fs: false,
+          // fixes mapbox dependencies
+          events: false,
+          // fixes sentry dependencies
+          process: false,
+        },
+      };
+    }
+    config.plugins.push(
+      new webpack.NormalModuleReplacementPlugin(/node:/, (resource) => {
+        resource.request = resource.request.replace(/^node:/, "");
+      })
+    );
+
+    return config;
+  },
+  images: {
+    formats: ['image/webp'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "humanaigc.github.io",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+  // experimental: {appDir: true}
+};
+
+module.exports = nextConfig;
